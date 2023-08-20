2ecccd307e50ddf9ea4049aff03875631d9d0c95
remvst <remvst@gmail.com>
2023-08-20 12:18:14 -0400

    Respawn moved to gameplay level

--- src/js/level/gameplay-level.js
+++ src/js/level/gameplay-level.js
@@ -40,5 +40,26 @@ class GameplayLevel extends Level {
 
             y += 100;
         }
+
+        // Respawn when far from the path
+        (async () => {
+            while (true) {
+                await this.scene.waitFor(() => abs(player.y - this.scene.pathCurve(player.x)) > 1000);
+
+                const fade = this.scene.add(new Fade());
+                await this.scene.add(new Interpolator(fade, 'alpha', 0, 1, 2)).await();
+                player.y = this.scene.pathCurve(player.x);
+                camera.cycle(999);
+                await this.scene.add(new Interpolator(fade, 'alpha', 1, 0, 2)).await();
+                fade.remove();
+            }
+        })();
+
+        // Scenario
+        (async () => {
+            const fade = this.scene.add(new Fade());
+            await this.scene.add(new Interpolator(fade, 'alpha', 1, 0, 2)).await();
+            fade.remove();
+        })();
     }
 }
--- src/js/level/level.js
+++ src/js/level/level.js
@@ -44,20 +44,6 @@ class Level {
             bush.x = random() * 10000;
             this.scene.add(bush);
         }
-
-        // Respawn when far from the path
-        (async () => {
-            while (true) {
-                await this.scene.waitFor(() => abs(player.y - this.scene.pathCurve(player.x)) > 1000);
-
-                const fade = this.scene.add(new Fade());
-                await this.scene.add(new Interpolator(fade, 'alpha', 0, 1, 2)).await();
-                player.y = this.scene.pathCurve(player.x);
-                camera.cycle(999);
-                await this.scene.add(new Interpolator(fade, 'alpha', 1, 0, 2)).await();
-                fade.remove();
-            }
-        })();
     }
 
     cycle(elapsed) {
