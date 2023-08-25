22eab9aa5dc1450084c68e1cb4653a4d5c09f13b
remvst <remvst@gmail.com>
2023-08-25 01:32:44 -0400

    Simplify rain rendering

--- src/js/entities/animations/rain.js
+++ src/js/entities/animations/rain.js
@@ -7,26 +7,22 @@ class Rain extends Entity {
         this.rng.reset();
 
         ctx.fillStyle = '#0af';
-        ctx.rotate(PI / 16);
+
+        this.cancelCameraOffset(camera);
 
         for (let i = 99 ; i-- ;) {
-            const age = this.age + this.rng.next(0, 10);
+            ctx.wrap(() => {
+                ctx.translate(evaluate(CANVAS_WIDTH / 2), evaluate(CANVAS_HEIGHT / 2));
+                ctx.rotate(this.rng.next(0, PI / 16));
+                ctx.translate(-evaluate(CANVAS_WIDTH / 2), -evaluate(CANVAS_HEIGHT / 2));
 
-            const leftX = camera.x - CANVAS_WIDTH / 2;
-            const topY = camera.y - CANVAS_HEIGHT / 2;
-
-            const x = this.rng.next(0, CANVAS_WIDTH);
-            const y = this.rng.next(1000, 2000) * age;
-
-            const shiftXCount = floor((x - leftX) / CANVAS_WIDTH);
-            const shiftYCount = floor((y - topY) / CANVAS_HEIGHT);
-
-            ctx.fillRect(
-                x - shiftXCount * CANVAS_WIDTH,
-                y - shiftYCount * CANVAS_HEIGHT,
-                2,
-                20,
-            );
+                ctx.fillRect(
+                    this.rng.next(0, CANVAS_WIDTH),
+                    this.rng.next(1000, 2000) * (this.age + this.rng.next(0, 10)) % CANVAS_HEIGHT,
+                    2,
+                    20,
+                );
+            });
         }
     }
 }
