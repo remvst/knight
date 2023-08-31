0265e284b776f40ba2ca01dbc6a71db04ce1ff1e
remvst <remvst@gmail.com>
2023-08-30 21:29:59 -0400

    Animate instruction changes

--- src/js/entities/ui/instruction.js
+++ src/js/entities/ui/instruction.js
@@ -4,12 +4,27 @@ class Instruction extends Entity {
         return LAYER_INSTRUCTIONS; 
     }
 
+    cycle(elapsed) {
+        super.cycle(elapsed);
+
+        if (this.text != this.previousText) {
+            this.previousText = this.text;
+            this.textAge = 0; 
+        }
+        this.textAge += elapsed;
+    }
+
     doRender(camera) {
         if (!this.text || GAME_PAUSED) return;
 
         this.cancelCameraOffset(camera);
 
         ctx.translate(CANVAS_WIDTH / 2, CANVAS_HEIGHT * 5 / 6);
+
+        ctx.scale(
+            interpolate(1.2, 1, this.textAge * 8),
+            interpolate(1.2, 1, this.textAge * 8),
+        );
         ctx.renderInstruction(this.text);
     }
 }
