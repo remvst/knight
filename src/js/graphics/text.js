c8f86b69848c79307ea71e5be4edd8766ae820ce
remvst <remvst@gmail.com>
2023-08-31 23:29:50 -0400

    More with(this)

--- src/js/graphics/text.js
+++ src/js/graphics/text.js
@@ -7,49 +7,53 @@ LOGO_GRADIENT = createCanvas(1, 1, (ctx) => {
 });
 
 canvasPrototype.renderLargeText = function (bits) {
-    this.textBaseline = nomangle('alphabetic');
-    this.textAlign = nomangle('left');
-    this.fillStyle = LOGO_GRADIENT;
-    this.strokeStyle = '#000';
-    this.lineWidth = 4;
-    this.shadowColor = '#000';
+    with (this) {
+        textBaseline = nomangle('alphabetic');
+        textAlign = nomangle('left');
+        fillStyle = LOGO_GRADIENT;
+        strokeStyle = '#000';
+        lineWidth = 4;
+        shadowColor = '#000';
 
-    let x = 0;
-    for (const [text, size, offsetWidth] of bits) {
-        this.font = size + nomangle('px Times New Roman');
-        x += this.measureText(text).width + (offsetWidth || 0);
+        let x = 0;
+        for (const [text, size, offsetWidth] of bits) {
+            font = size + nomangle('px Times New Roman');
+            x += measureText(text).width + (offsetWidth || 0);
+        }
+
+        translate(-x / 2, 0);
+
+        x = 0;
+        for (const [text, size, offsetWidth] of bits) {
+            font = size + nomangle('px Times New Roman');
+
+            shadowBlur = 5;
+            strokeText(text, x, 0);
+
+            shadowBlur = 0;
+            fillText(text, x, 0);
+
+            x += measureText(text).width + (offsetWidth || 0);
+        }
+
+        return x;
     }
-
-    this.translate(-x / 2, 0);
-
-    x = 0;
-    for (const [text, size, offsetWidth] of bits) {
-        this.font = size + nomangle('px Times New Roman');
-
-        this.shadowBlur = 5;
-        this.strokeText(text, x, 0);
-
-        this.shadowBlur = 0;
-        this.fillText(text, x, 0);
-
-        x += this.measureText(text).width + (offsetWidth || 0);
-    }
-
-    return x;
 };
 
 canvasPrototype.renderInstruction = function(text) {
-    this.textBaseline = nomangle('middle');
-    this.textAlign = nomangle('center');
-    this.strokeStyle = '#000';
-    this.lineWidth = 4;
-    this.font = nomangle('18pt Times New Roman');
+    with (this) {
+        textBaseline = nomangle('middle');
+        textAlign = nomangle('center');
+        strokeStyle = '#000';
+        lineWidth = 4;
+        font = nomangle('18pt Times New Roman');
 
-    const width = this.measureText(text).width + 20;
-    this.fillStyle = 'rgba(0,0,0,.5)';
-    this.fillRect(-width / 2, 0, width, 40);
+        const width = measureText(text).width + 20;
+        fillStyle = 'rgba(0,0,0,.5)';
+        fillRect(-width / 2, 0, width, 40);
 
-    this.fillStyle = '#fff';
-    this.strokeText(text, 0, 20);
-    this.fillText(text, 0, 20);
+        fillStyle = '#fff';
+        strokeText(text, 0, 20);
+        fillText(text, 0, 20);
+    }
 };
