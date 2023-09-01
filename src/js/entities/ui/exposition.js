e127689a8d40666c889e533817c75b1093fbc05c
remvst <remvst@gmail.com>
2023-08-31 22:04:27 -0400

    Increase exposition font size

--- src/js/entities/ui/exposition.js
+++ src/js/entities/ui/exposition.js
@@ -15,19 +15,19 @@ class Exposition extends Entity {
 
         this.cancelCameraOffset(camera);
 
-        ctx.translate(200, evaluate(CANVAS_HEIGHT / 2));
+        ctx.translate(150, evaluate(CANVAS_HEIGHT / 2));
 
         ctx.textBaseline = nomangle('middle');
         ctx.textAlign = nomangle('left');
         ctx.fillStyle = '#fff';
-        ctx.font = nomangle('18pt Times New Roman');
+        ctx.font = nomangle('24pt Times New Roman');
 
         let y = -this.text.length / 2 * 50;
         let lineIndex = 0;
         for (const line of this.text) {
             ctx.globalAlpha = between(0, (this.age - lineIndex * 3), 1) * this.alpha;
             ctx.fillText(line, 0, y);
-            y += 50;
+            y += 75;
             lineIndex++;
         }
     }
